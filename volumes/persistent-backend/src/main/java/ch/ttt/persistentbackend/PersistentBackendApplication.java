b7d26b727452b5f5f19fbd02d41112b31ed764b9
ttt <tschachtli.th@gmail.com>
2018-12-23 22:57:53 +0100

    volumes

---
+++ volumes/persistent-backend/src/main/java/ch/ttt/persistentbackend/PersistentBackendApplication.java
@@ -0,0 +1,14 @@
+package ch.ttt.persistentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistentBackendApplication.class, args);
+	}
+
+}
+
