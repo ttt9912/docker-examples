e4ed572c489160fdb91056955d30e9659c26eabb
ttt <tschachtli.th@gmail.com>
2018-12-25 00:35:49 +0100

    compose

---
+++ compose/db-backend/src/main/java/ch/ttt/dbbackend/DbBackendApplication.java
@@ -0,0 +1,14 @@
+package ch.ttt.dbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbBackendApplication.class, args);
+	}
+
+}
+
---
+++ compose/db-backend/src/main/java/ch/ttt/dbbackend/EntryRepository.java
@@ -0,0 +1,4 @@
+package ch.ttt.dbbackend;
+
+public class EntryRepository {
+}
